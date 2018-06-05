85642e359d3a0a8c32b98bcdc39fc55dbdb549f5
Flavio Brasil <fbrasil@twitter.com>
2018-06-05 15:52:41 -0700

    wip

--- compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/fusing/FusingPhase.java
+++ compiler/src/org.graalvm.compiler.phases.common/src/org/graalvm/compiler/phases/common/fusing/FusingPhase.java
@@ -12,9 +12,11 @@ import org.graalvm.compiler.debug.DebugContext;
 import org.graalvm.compiler.graph.Node;
 import org.graalvm.compiler.graph.NodeSourcePosition;
 import org.graalvm.compiler.nodes.CallTargetNode.InvokeKind;
+import org.graalvm.compiler.nodes.FrameState;
 import org.graalvm.compiler.nodes.InvokeNode;
 import org.graalvm.compiler.nodes.StructuredGraph;
 import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.nodes.graphbuilderconf.GraphBuilderTool;
 import org.graalvm.compiler.nodes.java.MethodCallTargetNode;
 import org.graalvm.compiler.phases.BasePhase;
 import org.graalvm.compiler.phases.common.fusing.Config.Entry;
@@ -23,6 +25,7 @@ import org.graalvm.compiler.phases.tiers.HighTierContext;
 import org.graalvm.compiler.phases.tiers.PhaseContext;
 
 import jdk.vm.ci.code.BytecodeFrame;
+import jdk.vm.ci.meta.JavaKind;
 import jdk.vm.ci.meta.JavaType;
 import jdk.vm.ci.meta.ResolvedJavaMethod;
 import jdk.vm.ci.meta.Signature;
@@ -47,11 +50,15 @@ public class FusingPhase extends BasePhase<HighTierContext> {
             graph.getDebug().dump(DebugContext.BASIC_LEVEL, graph, "before fusing " + entry.getFusingClass());
 
             roots.forEach(invoke -> {
+
+                ResolvedJavaMethod outerMethod = invoke.stateAfter().getMethod();
+
                 ResolvedJavaMethod stageMethod = context.getMetaAccess().lookupJavaMethod(entry.getStageMethod());
                 InvokeNode stageInvoke = createInvoke(graph, stageMethod, InvokeKind.Static, invoke.callTarget().arguments().first());
-
                 stageInvoke.setUseForInlining(true);
-                stageInvoke.setStateAfter(invoke.stateAfter());
+
+                setFrameState(graph, outerMethod, stageInvoke);
+
                 invoke.replaceAtPredecessor(stageInvoke);
                 stageInvoke.replaceFirstSuccessor(null, invoke);
 
@@ -59,8 +66,19 @@ public class FusingPhase extends BasePhase<HighTierContext> {
 
                 InvokeNode curr = invoke;
                 while (true) {
-                    curr.callTarget().setTargetMethod(fusingMap.get(curr));
-                    curr.setUseForInlining(true);
+
+                    ResolvedJavaMethod method = fusingMap.get(curr);
+                    Signature signature = method.getSignature();
+                    JavaType returnType = signature.getReturnType(null);
+                    StampPair returnStamp = StampFactory.forDeclaredType(graph.getAssumptions(), returnType, false);
+                    MethodCallTargetNode callTarget = new MethodCallTargetNode(InvokeKind.Virtual, method, curr.callTarget().arguments().toArray(new ValueNode[0]), returnStamp, null);
+                    curr.callTarget().replaceAndDelete(callTarget);
+                    InvokeNode newInvoke = graph.addOrUniqueWithInputs(new InvokeNode(callTarget, BytecodeFrame.BEFORE_BCI));
+
+                    setFrameState(graph, outerMethod, newInvoke);
+
+                    graph.replaceFixedWithFixed(curr, newInvoke);
+                    curr = newInvoke;
                     if (fusingMap.containsKey(curr.next()))
                         curr = (InvokeNode) curr.next();
                     else
@@ -72,7 +90,9 @@ public class FusingPhase extends BasePhase<HighTierContext> {
 
                 Node next = curr.next();
                 fuseInvoke.setUseForInlining(true);
-                fuseInvoke.setStateAfter(curr.stateAfter());
+
+                setFrameState(graph, outerMethod, fuseInvoke);
+
                 next.replaceAtPredecessor(fuseInvoke);
                 next.replaceFirstInput(curr, fuseInvoke);
                 fuseInvoke.replaceFirstSuccessor(null, next);
@@ -80,6 +100,17 @@ public class FusingPhase extends BasePhase<HighTierContext> {
             graph.getDebug().dump(DebugContext.BASIC_LEVEL, graph, "after fusing " + entry.getFusingClass());
             inliningPhase.apply(graph, context);
         });
+    }
+
+    private void setFrameState(StructuredGraph graph, ResolvedJavaMethod method, InvokeNode stageInvoke) {
+        FrameStateBuilder frameStateBuilder = new FrameStateBuilder(method, graph);
+        if (stageInvoke.getStackKind() != JavaKind.Void) {
+            frameStateBuilder.push(stageInvoke.getStackKind(), stageInvoke);
+        }
+        stageInvoke.setStateAfter(frameStateBuilder.create(BytecodeFrame.UNKNOWN_BCI, stageInvoke));
+        if (stageInvoke.getStackKind() != JavaKind.Void) {
+            frameStateBuilder.pop(stageInvoke.getStackKind());
+        }
     }
 
     private static Set<InvokeNode> roots(Map<InvokeNode, ResolvedJavaMethod> fusingMap) {
